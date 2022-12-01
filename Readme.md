fa4adf62615b872bcd103e9001b0569d745522f7
peteris racinskis <peterisracinskis@gmail.com>
2022-12-01 11:28:02 +0200

    initial writeup

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
-## RL Playground - simple examples of agent models in a Markov Decision Process
+## MDP reference - simple examples of agents in a Markov Decision Process
 
 ### QMatrix - an implementation of the classic Bellman equation through a fully enumerated reward matrix
+
+The simplest, "brute force" approach to reinforcement learning. Construct a tensor the dimensions of which correspond to discretized versions of the states and discrete action set. Every value corresponds to the expected reward. Output an action based on maximum expected reward at any given state. Iteratively optimize the values in the tensor by repeatedly sampling the environment and using Bellman's update rule. 
 
 ### ActorCritic - a simple deep learning approach using the Advantage Actor-Critic architecture
 
--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
 import gym
 
-def visualize_model(model, envname="CartPole-v1", max_steps = 2000, episodes = 100):
-    env = gym.make(envname, render_mode = "human")
+def visualize_model(model, envname="CartPole-v1", max_steps = 2000, episodes = 100, notebook = False):
+    if notebook:
+        import matplotlib.pyplot as plt
+        from IPython.display import clear_output
+        episodes = 1
+        env = gym.make(envname, render_mode = "rgb_array")
+    else:
+        env = gym.make(envname, render_mode = "human")
 
     for ep in range(episodes):    
         state, _ = env.reset()
@@ -11,6 +17,10 @@ def visualize_model(model, envname="CartPole-v1", max_steps = 2000, episodes = 1
             action = model.get_action(state)
             state, _, done, __, __ = env.step(action)
             steps += 1
+            if notebook:
+                clear_output(wait=True)
+                plt.imshow(env.render())
+                plt.show()
         print(f"episode {ep} died at {steps}")
 
     
